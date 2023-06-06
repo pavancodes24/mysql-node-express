83b9904e90827c5de30373b9ed73f9495bf05047
pavan <pavanchandupatla24@gmail.com>
2023-06-06 22:29:43 +0530

    init

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(errorMiddleware);
 
 // starting the server
 app.listen(port, () =>
-    console.log(`🚀 Server running on port ${port}!`));
+    console.log(`🚀 Server running on port ${port}! testing`));
 
 
 module.exports = app;
